2e413c840532242f5e4a2901f8926c401b567d7e
Yi Xinglu <yxlyoo@gmail.com>
2014-06-30 22:57:36 +0800

    vector insert performance and shared_ptr in vector

---
+++ CodeSnippet/clear.bat
@@ -0,0 +1,3 @@
+@echo off
+DEL *.exe
+DEL *.obj
--- CodeSnippet/shared_ptr.cpp
+++ CodeSnippet/shared_ptr.cpp
@@ -1,11 +1,35 @@
 #include <iostream>
+#include <vector>
 #include <memory>
+#include <time.h>
+
+using namespace std;
 
 struct Base {
   int i;
 };
 
-using namespace std;
+struct Drive : public Base {};
+
+typedef shared_ptr<Base> base_ptr;
+typedef shared_ptr<Drive> drive_ptr;
+
+void test_performance() {
+  vector<base_ptr> vb, vb1;
+  size_t sz = 10000000;
+  vector<drive_ptr> vd(sz);
+
+  auto t = clock();
+  vb.reserve(sz);
+  for (size_t i = 0; i < sz; ++i) {
+    vb.push_back(vd[i]);
+  }
+  cout << (double)(clock()-t)/CLOCKS_PER_SEC << endl;
+
+  t = clock();
+  vb1.insert(vb1.begin(), vd.begin(), vd.end());
+  cout << (double)(clock()-t)/CLOCKS_PER_SEC << endl;
+}
 
 int main(int argc, char* argv[]) {
   auto ptr = make_shared<Base>();
@@ -15,6 +39,13 @@ int main(int argc, char* argv[]) {
   shared_ptr<Base> ptr2(new Base);
   ptr2->i = 11;
   cout << ptr2->i << endl;
+
+  vector<base_ptr> vb;
+  vb.push_back(ptr);
+  ptr->i = 100;
+  cout << vb.back()->i << endl;
+
+  test_performance();
 
   return 0;
 }
