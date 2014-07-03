c2cd34a3fdf172ec883f5de47f80ced0054995b1
Yi Xinglu <yxlyoo@gmail.com>
2014-07-03 21:01:45 +0800

    format

--- CodeSnippet/mutual_ref.cpp
+++ CodeSnippet/mutual_ref.cpp
@@ -5,25 +5,25 @@ using namespace std;
 
 struct Bar;
 struct Foo{
-	shared_ptr<Bar> bar;	
+  shared_ptr<Bar> bar;	
 };
 
 struct Bar {
-	shared_ptr<Foo> foo;
+  shared_ptr<Foo> foo;
 };
 
 void test() {
-	//auto fptr = make_shared<Foo>();
-	//auto bptr = make_shared<Bar>();
+  //auto fptr = make_shared<Foo>();
+  //auto bptr = make_shared<Bar>();
   shared_ptr<Foo> fptr(new Foo());
   shared_ptr<Bar> bptr(new Bar());
-	fptr->bar = bptr;
-	bptr->foo = fptr;
+  fptr->bar = bptr;
+  bptr->foo = fptr;
 }
 
 int main() {
-	test();
+  test();
 
-	MEM_CHK_EXIT_DBG
-	return 0;
+  MEM_CHK_EXIT_DBG
+  return 0;
 }
