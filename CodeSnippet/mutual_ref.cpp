42c4ea5a560b200e44e10ebe42decae452082c6c
Yi Xinglu <yxlyoo@gmail.com>
2014-07-03 23:45:15 +0800

    weak_ptr

--- CodeSnippet/mutual_ref.cpp
+++ CodeSnippet/mutual_ref.cpp
@@ -1,4 +1,5 @@
 #include <memory>
+#include <iostream>
 #include "memchk.h"
 
 using namespace std;
@@ -12,6 +13,21 @@ struct Bar {
   shared_ptr<Foo> foo;
 };
 
+struct Bar2 {
+  weak_ptr<Foo> foo;
+};
+
+shared_ptr<Bar2> b2ptr(new Bar2());
+
+void check_expired(const weak_ptr<Foo>& ptr) {
+  if (ptr.expired()) {
+    cout << "expired" << endl;
+  }
+  else {
+    cout << "not expired" << endl;
+  }
+}
+
 void test() {
   //auto fptr = make_shared<Foo>();
   //auto bptr = make_shared<Bar>();
@@ -19,10 +35,14 @@ void test() {
   shared_ptr<Bar> bptr(new Bar());
   fptr->bar = bptr;
   bptr->foo = fptr;
+  auto f2ptr = make_shared<Foo>();
+  b2ptr->foo = f2ptr;
+  check_expired(b2ptr->foo);
 }
 
 int main() {
   test();
+  check_expired(b2ptr->foo);
 
   MEM_CHK_EXIT_DBG
   return 0;
