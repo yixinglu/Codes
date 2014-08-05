865ffd8c4ddd86ee7eccdabeea327a56b5d5762e
Yi Xinglu <yxlyee@gmail.com>
2014-08-06 01:19:12 +0800

    Update high_order_fun.py

--- Python教程/ch05 functional programming/high_order_fun.py
+++ Python教程/ch05 functional programming/high_order_fun.py
@@ -23,6 +23,7 @@ print isinstance(str2int('13579'), int)
 
 def str2int(s):
   return reduce(lambda x,y: x*10+y, map(int, s))
+  
 print isinstance(str2int('13579'), int)
 
 print sorted([36, 5, 12, 9, 21])
