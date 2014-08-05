f0e531593b88c0b63289c78cd889f19f83021868
Yi Xinglu <yxlyoo@gmail.com>
2014-08-06 01:24:36 +0800

    anonymous function

--- Python教程/ch05 functional programming/anonymous_fun.py
+++ Python教程/ch05 functional programming/anonymous_fun.py
@@ -2,6 +2,13 @@
 
 print map(lambda x: x*x, [1, 2, 3, 4, 5, 6, 7, 8, 9])
 
+def f(x):
+  return x * x
+
+f = lambda x: x * x
+print f
+print f(5)
+
 def build(x, y):
   return lambda: x * x + y * y
 
