cb676c0b6034a40c4a1c2ecaa4b47da4bc53f719
yixinglu <yxlyee@gmail.com>
2016-08-29 21:12:16 +0800

    test spacemacs code style

---
+++ DesignPatterns/bridge.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+// ssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss
+// sssssssssssssssssssssssssssssssssss
+
+class Foo {
+ public:
+  Foo() {
+    cout << "helldafasdfasfasdfasdfassdfsafaadfasdfaaaaaasssssssssssso world"
+         << endl;
+  }
+  void sayaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa(
+      int adfaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaafs,
+      long fasfdffffffffffffffffffffsfsf) {
+    cout << "helloworld" << endl;
+  }
+
+ private:
+  string hellow_;
+};
+
+int main() {
+  Foo foo;
+  return 0;
+}
